ff6f04f9f26988bf037efa1e4fabcdfe32ca02c2
Марат <marat@MacBook-Pro-5.local>
2026-06-22 18:21:21 +0300

    fix: vite config for socket.io-client

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['socket.io-client']
+  },
+  build: {
+    commonjsOptions: {
+      include: [/socket\.io-client/, /node_modules/]
+    }
+  }
 })
